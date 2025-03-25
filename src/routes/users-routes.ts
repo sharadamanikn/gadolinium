661dc45874d9d74ab96b3ba9dfa832ed136a5c80
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-25 19:02:20 +0530

    Refactor user routes to use getAllUsers controller and improve error handling

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { prismaClient } from "../extras/prisma";
 import { tokenMiddleware } from "./middlewares/token-middleware";
-import { getMe } from "../controllers/users/users-controllers";
+import { getAllUsers, getMe } from "../controllers/users/users-controllers";
 import { GetMeError } from "../controllers/users/users-types";
 
 export const usersRoutes = new Hono();
@@ -39,10 +39,26 @@ usersRoutes.get("/me", tokenMiddleware, async (context) => {
   }
 });
 
-usersRoutes.get("/getAllUsers", tokenMiddleware, async (context) => {
-  const users = await prismaClient.user.findMany();
-return context.json({data:users})
-  return context.json(users, 200);
+
+// /users
+usersRoutes.get("", tokenMiddleware, async (context) => {
+  try {
+    const users = await getAllUsers();
+
+    return context.json(
+      {
+        data: users,
+      },
+      200
+    );
+  } catch (e) {
+    return context.json(
+      {
+        message: "Internal Server Error",
+      },
+      500
+    );
+  }
 });
 
 
