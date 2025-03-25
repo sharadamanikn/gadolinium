9db5ec1b737aee9de16dd3a6f3277f828e4f3ff7
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-25 13:00:05 +0530

    Add routing setup for authentication and user endpoints with health check

---
+++ src/routes/routes.ts
@@ -0,0 +1,45 @@
+import { Hono } from "hono";
+import {
+  LogInWtihUsernameAndPasswordError,
+  SignUpWithUsernameAndPasswordError,
+} from "../controllers/authentication/authentication-types";
+import { auth } from "hono/utils/basic-auth";
+import { authenticationRoutes } from "./authentication-routes";
+import { userRoutes } from "./users-routes";
+
+
+
+export const allRoutes = new Hono();
+import { prismaClient } from "../extras/prisma";
+import jwt from "jsonwebtoken";
+import { jwtSecretKey } from "../../environment";
+
+
+
+allRoutes.use(async (context, next) => {
+console.log("HTTP Method ", context.req.method);
+console.log("URL ", context.req.url);
+console.log("Headers ", context.req.header());
+
+await next()
+});
+
+allRoutes.route("/authentication", authenticationRoutes);
+allRoutes.route("/users", userRoutes);
+
+
+
+allRoutes.get("/health", 
+   //Inline middleware
+//    async(context, next) => {
+//     console.log("Health Check Middleware");
+//      await next();
+//   },
+  async (c) => {
+  console.log("Health checked"); 
+   return c.json({ status: " ALLLL ok" });
+});
+
+
+
+
