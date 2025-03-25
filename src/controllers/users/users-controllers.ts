c02f84f665788cc5d27c42d3c85f05f7e4d94cbb
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-25 16:42:36 +0530

    Add user controller functions for fetching current user and all users

---
+++ src/controllers/users/users-controllers.ts
@@ -0,0 +1,36 @@
+import { prismaClient } from "../../extras/prisma";
+import { GetMeError, userError, type GetMeResult } from "./users-types";
+
+export const getMe = async (parameters: { userId: string }): Promise<GetMeResult> => {
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: parameters.userId,
+    },
+  });
+
+  if (!user) {
+    throw GetMeError.BAD_REQUEST;
+  }
+
+  return {
+    user,
+  };
+};
+
+import { type userResult } from "./users-types";
+
+export const getAllUsers = async (parameters: {}): Promise<userResult> => {
+    const users = await prismaClient.user.findMany();
+    if (!users) {
+      throw userError.BAD_REQUEST;
+    }
+    return {
+      users,
+    };
+  };
+
+
+  
+
+
+
